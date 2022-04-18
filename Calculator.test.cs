a402824ed093c31c02b6c605ad43e757da50f082
AlphaTango101 <86611647+AlphaTango101@users.noreply.github.com>
2022-04-18 22:29:39 +0200

    Add files via upload

---
+++ Calculator.test.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Calculator;
+
+namespace calc.test
+{
+    public class CalculatorTest
+    {
+        //Addition
+        [Theory]
+        [InlineData(26, 14, 12)]
+        [InlineData(0, 0, 0)]
+        [InlineData(14, 14, 0)]
+        [InlineData(0, 14, -14)]
+        [InlineData(26, 38, -12)]
+        public void addCheck(double expected, double num1, double num2)
+        {
+            double result = Calculator_.add( num1, num2 );
+            Assert.Equal(expected, result);
+        }
+        //Addition with array
+        [Theory]
+        [MemberData(nameof(additionData))]
+        public void addArrayCheck(double expected, params double[] addArray)
+        {
+            double num1 = addArray[0];
+            double num2 = 0;
+            for (int i = 1; i < addArray.Length; i++)
+            {
+                num2 += addArray[i];
+            }
+            double result = Calculator_.add(num1, num2);
+            Assert.Equal(expected, result);
+        }
+        public static IEnumerable<object[]> additionData()
+        {
+            yield return new object[] { 12, new double[] { 1, 2, 3, 4, 3, -1 } };
+            yield return new object[] { 12, new double[] { 6, 6 } };
+            yield return new object[] { 12, new double[] { 12, 0, 0, 0 } };
+        }
+
+        //Subtraction
+        [Theory]
+        [InlineData(2, 14, 12)]
+        [InlineData(0, 0, 0)]
+        [InlineData(14, 14, 0)]
+        [InlineData(28, 14, -14)]
+        [InlineData(50, 38, -12)]
+        public void subCheck(double expected, double num1, double num2)
+        {
+            double result = Calculator_.sub(num1, num2);
+            Assert.Equal(expected, result);
+        }
+        //Subtraction with array
+        [Theory]
+        [MemberData(nameof(subtractionData))]
+        public void subArrayCheck(double expected, params double[] subArray)
+        {
+            double num1 = subArray[0];
+            double num2 = 0;
+            for (int i = 1; i < subArray.Length; i++)
+            {
+                num2 += subArray[i];
+            }
+            double result = Calculator_.sub(num1, num2);
+            Assert.Equal(expected, result);
+        }
+        public static IEnumerable<object[]> subtractionData()
+        {
+            yield return new object[] { -10, new double[] { 1, 2, 3, 4, 3, -1 } };
+            yield return new object[] { 0, new double[] { 6, 6 } };
+            yield return new object[] { 12, new double[] { 12, 0, 0, 0 } };
+        }
+
+        //Multiplication
+        [Theory]
+        [InlineData(144, 12, 12)]
+        [InlineData(0, 0, 0)]
+        [InlineData(0, 14, 0)]
+        [InlineData(-144, 12, -12)]
+        [InlineData(36, -6, -6)]
+        public void multCheck(double expected, double num1, double num2)
+        {
+            double result = Calculator_.mult(num1, num2);
+            Assert.Equal(expected, result);
+        }
+
+        //Division
+        [Theory]
+        [InlineData(1, 12, 12)]
+        [InlineData(0, 0, 14)]
+        [InlineData(0, 14, 0)]
+        [InlineData(-1, 14, -14)]
+        [InlineData(1, -12, -12)]
+        public void divCheck(double expected, double num1, double num2)
+        {
+            double result = Calculator_.div(num1, num2);
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Calculator
+{
+    public static class Calculator_
+    {
+        static void old(string[] args)
+        //variable declarations
+        {
+            char op;
+            bool runCalc = true;
+            double num1, num2;
+
+            while (runCalc)
+            {
+                Console.WriteLine("Enter the Operator( +,-,*,/ )");
+                op = Console.ReadLine()[0];
+
+                Console.WriteLine("Enter the two numbers one by one");
+                num1 = Convert.ToDouble(Console.ReadLine());
+                num2 = Convert.ToDouble(Console.ReadLine());
+
+                switch (op)
+                {
+                    case '+':
+                        add(num1, num2);
+                        break;
+
+                    case '-':
+                        sub(num1, num2);
+                        break;
+
+                    case '*':
+                        mult(num1, num2);
+                        break;
+
+                    case '/':
+                        div(num1, num2);
+                        break;
+
+                }
+                runCalc = run();
+            }
+        }
+
+        //Addition
+        public static double add(double num1, double num2)
+        {
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, (num1 + num2));
+            return (num1 + num2);
+        }
+
+        //Subtraction
+        public static double sub(double num1, double num2)
+        {
+            Console.WriteLine("{0} - {1} = {2}", num1, num2, (num1 - num2));
+            return (num1 - num2);
+        }
+        //Multiplication
+        public static double mult(double num1, double num2)
+        {
+            Console.WriteLine("{0} * {1} = {2}", num1, num2, (num1 * num2));
+            return (num1 * num2);
+        }
+        //Division
+        public static double div(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                Console.WriteLine("Divide by zero not possible");
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine("{0} / {1} = {2}", num1, num2, (num1 / num2));
+                return (num1 / num2);
+            }
+        }
+
+        //continue?
+        static bool run()
+        {
+            Console.WriteLine("press ENTER to continue.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("do you wish to perform a new calculation? (yes/no)");
+            string input = Console.ReadLine().ToLower();
+            while (input != "yes" && input != "no")
+            {
+                Console.WriteLine("What you had input was not a yes or a no, please input an accepted answer.");
+                input = Console.ReadLine().ToLower();
+            }
+            if (input == "yes") return true;
+            return false;
+        }
+
+    }
+}
